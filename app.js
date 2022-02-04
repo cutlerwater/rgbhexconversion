c26ad656ed26cbf07ea94f1445dda8e4a9fe0415
cutlerwater <cutlerwater2@live.com>
2022-02-04 10:55:27 -0500

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-const jokeEl = document.getElementById('joke');
-const get_joke = document.getElementById('get_joke');
-
-get_joke.addEventListener('click', generateJoke);
-
-generateJoke();
-
-async function generateJoke() {
-	const jokeRes = await fetch('https://icanhazdadjoke.com/', {
-		headers: {
-			'Accept': 'application/json'
-		}
-	});
-	const joke = await jokeRes.json();
-	jokeEl.innerHTML = joke.joke;
+function colorAlphaAjusted(c, a) {
+	// against white background
+	return c * a + 255 * (1 - a);
 }
 
+function rgbaToRgb(r, g, b, a) {
+	return [colorAlphaAjusted(r, a), colorAlphaAjusted(g, a), colorAlphaAjusted(b, a) ];
+}
+
+function componentToHex(c) {
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+
+function convert(e) {
+	e.preventDefault();
+	var red = document.getElementById('red');
+	var green = document.getElementById('green');
+	var blue = document.getElementById('blue');
+	var alpha = document.getElementById('alpha');
+	var rValue = red && red.value;
+	var gValue = green && green.value;
+	var bValue = blue && blue.value;
+	var aValue = alpha && alpha.value;
+	aValue /= 100;
+	var [r, g, b] = rgbaToRgb(rValue, gValue, bValue, aValue);
+	document.getElementById('result').innerHTML = "#" + componentToHex(Math.round(r)) + componentToHex(Math.round(g)) + componentToHex(Math.round(b));
+	return false;
+}
 
 
 
